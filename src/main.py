9260dcfca7b3eeed7529396b9684039b355ede48
ole <s92933@bht-berlin.de>
2026-03-17 13:33:12 +0100

    cors config fixed

--- src/main.py
+++ src/main.py
@@ -9,9 +9,15 @@ from src import database
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8080"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
