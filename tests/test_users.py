c23aad8fd5076eb4f0e5de3e95c2374bfcaa5c43
ole <s92933@bht-berlin.de>
2026-03-20 00:22:25 +0100

    more tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -28,8 +28,18 @@ def client(mock_collections: dict):
         from src.main import app
         yield TestClient(app), mock_collections
 
+@pytest.fixture(scope="function")
+def test_client(client) -> TestClient:
+    http_client, _ = client
+    return http_client
+
 ALICE = MOCKUP_OBJECTS.user_alice
 BOB = MOCKUP_OBJECTS.user_bob
+CARL = {
+    "name": "Carl",
+    "email": "carl@example.com",
+    "password": "carl123"
+}
 
 ALICE_PUBLIC = {
     "userId": ALICE.userId,
@@ -39,8 +49,64 @@ ALICE_PUBLIC = {
 }
 
 class TestGetUsers:
-    def test_returns_empty_list_when_no_users(self, client):
-        test_client, _ = client
+    # empty user list
+    def test_empty_user_list(self, test_client: TestClient):
         response = test_client.get("/api/users")
         assert response.status_code == 200
         assert response.json() == []
+    
+    # filled user list
+    def test_filled_user_list(self, test_client: TestClient):
+        test_client.post("/api/user", json=CARL)
+        response = test_client.get("/api/users")
+        assert response.status_code == 200
+    
+    # get user by name
+    def test(self, test_client: TestClient):
+        test_client.post("/api/user", json=CARL)
+        response = test_client.get(f"/api/user/{CARL["name"]}")
+        assert response.status_code == 200
+    
+    # get user by email
+    def test(self, test_client: TestClient):
+        test_client.post("/api/user", json=CARL)
+        return
+
+class TestPostUsers:
+    # new user
+    def test_new_user(self, test_client: TestClient):
+        response = test_client.post("/api/signup", json=CARL)
+        assert response.status_code == 201
+    
+    # error existing user
+    def test_error_existing_user(self, test_client: TestClient):
+        test_client.post("/api/signup", json=CARL)
+        response = test_client.post("/api/signup", json=CARL)
+        assert response.status_code == 409
+    
+    # error incomplete user
+    def test_error_incomplete_user(self, test_client: TestClient):
+        response = test_client.post("/api/signup", json={"name": "Carl"})
+        assert response.status_code == 400
+
+class TestPutUsers:
+    # update user
+    def test(self, test_client: TestClient):
+        return
+    
+    # error not existing user
+    def test(self, test_client: TestClient):
+        return
+    
+    # error incomplete user
+    def test(self, test_client: TestClient):
+        return
+
+class TestDeleteUsers:
+    # delete user
+    def test(self, test_client: TestClient):
+        return
+    
+    # error not existing user
+    def test(self, test_client: TestClient):
+        return
