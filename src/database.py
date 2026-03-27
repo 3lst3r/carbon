cd45890b5d25b18611186bdd340d50c6c70c1203
ole <s92933@bht-berlin.de>
2026-03-27 18:54:40 +0100

    JWT hotfix

--- src/database.py
+++ src/database.py
@@ -424,7 +424,7 @@ def get_all_categories():
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return token
